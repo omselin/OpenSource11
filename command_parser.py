f115936c6bb9385c08dc13b76b0a3262dd8b67c1
chocolate2003 <chocolate2003@naver.com>
2025-05-26 00:45:53 +0900

    scramble_map_example

--- command_parser.py
+++ command_parser.py
@@ -41,7 +41,7 @@ def parse_and_execute_command(map:'Map',variable_map:'VariableMap',code, pos:tup
 # ⬇️ 오유민님 작업 시작 위치 (이 아래에만 작성해 주세요. 이 주석은 나중에 병합 기준이 되므로 수정하지 마세요.)
     if code.startswith('scramble(') and code.endswith(')'):
         arg = code[9:-1].strip().strip('"').strip("'")
-        return ce.yumin_scramble(map, [arg], None)
+        return ce.scramble_oh(map, [arg], None)
 
 # ⬇️ 이기상님 작업 시작 위치 (이 아래에만 작성해 주세요. 이 주석은 나중에 병합 기준이 되므로 수정하지 마세요.)
     if code.startswith('swap(') and code.endswith(')'):
